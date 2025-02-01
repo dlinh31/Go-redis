767ab76145bb7fd6365f494fc46f306ac71bec83
Liam Ha <haduylinh2005.work@gmail.com>
2025-02-01 18:02:40 -0500

    add func for reading RESP

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:6379")
+	fmt.Println("Listening on port :6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil{
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(value)
+
+		conn.Write([]byte("+OK\r\n"))
+	}
+
+
+	
+}
